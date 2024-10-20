2a670398e709e7eaa86df51b4c1f0e7e04f6ee2a
AyaOmer <100145751+AyaOmer@users.noreply.github.com>
2024-10-20 17:42:46 +0300

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -13,6 +13,12 @@ resource "aws_security_group" "SG" {
     protocol  = "tcp"
     cidr_blocks  = ["0.0.0.0/0"]
   }
+ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
 
   ingress {
     from_port = 5000
